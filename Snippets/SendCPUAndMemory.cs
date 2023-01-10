4b6395c1e585938f98bc93312ae933c5767cc4a6
terrymandin <terry.mandin@microsoft.com>
2023-01-10 09:18:09 -0700

    Create SendCPUAndMemory.cs

---
+++ Snippets/SendCPUAndMemory.cs
@@ -0,0 +1,24 @@
+# dotnet add package System.Diagnostics.PerformanceCounter --version 7.0.0
+
+
+using System.Diagnostics;
+
+Console.WriteLine("Sending 1,000 telemetry messages 5 seconds apart ...");
+PerformanceCounter cpuCounter;
+PerformanceCounter ramCounter;
+cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+for (int i = 0; i<100; i++)
+{
+    var jsonMessage = new
+    {
+        Computer = System.Environment.MachineName,
+        TimeStamp = DateTime.Now,
+        CPU = cpuCounter.NextValue()+"%",
+        Memory = ramCounter.NextValue()+"MB"
+    };
+    Console.WriteLine("SENDING - " + jsonMessage.ToString());
+    using var message = new Message(Encoding.UTF8.GetBytes(jsonMessage.ToString()));
+    await iotClient.SendEventAsync(message);
+    System.Threading.Thread.Sleep(5000);
+}
